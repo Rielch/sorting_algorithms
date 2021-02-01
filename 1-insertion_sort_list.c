32ce5ab7a4dacc61cce2e32d2ddba001975f2798
Gabriel Prochnau <2288@holbertonschool.com>
2021-02-01 16:36:53 +0000

    Add part 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list using the inserion sort
+ *
+ * @list: doubly linked list
+ * Return: Void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp = *list, *actual, *change;
+
+	if (list != NULL)
+	{
+		while (temp->prev != NULL)
+			temp = temp->prev;
+		*list = temp;
+		actual = temp->next;
+		while (actual != NULL)
+		{
+			change = actual->prev;
+			temp = actual->next;
+			while (change->n > actual->n)
+			{
+				if (actual->next != NULL)
+					actual->next->prev = change;
+				if (change->prev != NULL)
+					change->prev->next = actual;
+				change->next = actual->next;
+				actual->prev = change->prev;
+				actual->next = change;
+				change->prev = actual;
+				if (actual->prev != NULL)
+					change = actual->prev;
+				else
+				{
+					*list = actual;
+					print_list(*list);
+					break;
+				}
+				print_list(*list);
+			}
+			actual = temp;
+		}
+	}
+}
