da13f5d92687985cb7cf844a00c1a9bceb8cbda8
Gabriel Prochnau <2288@holbertonschool.com>
2021-02-04 12:02:57 +0000

    add

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *temp = *list, *actual, *change;
 
-	if (!list || !(*list) || (!(*list)->next))
+	if (list == NULL || *list == NULL)
 		return;
 
 	actual = temp->next;
